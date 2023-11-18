a5b1b6a00792c3b59b67cdb2f963089c0a507a1d
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-11-18 19:24:00 +0200

    updates 4.tsx

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -5,6 +5,17 @@ type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
 
 // Додати тип Menu Selected
+type SelectedMenu = {
+  id?: MenuIds;
+}
+
+type MenuSelected = {
+  selectedMenu: SelectedMenu
+}
+
+type MenuAction = {
+  onSelectedMenu: (menu: SelectedMenu) => void
+}
 
 const MenuSelectedContext = createContext<MenuSelected>({
   selectedMenu: {},
@@ -17,7 +28,7 @@ const MenuActionContext = createContext<MenuAction>({
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+  children: React.ReactNode; // Додати тип для children
 };
 
 function MenuProvider({ children }: PropsProvider) {
@@ -48,7 +59,7 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[]; // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
